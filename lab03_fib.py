25a073926f8f770f606e67bb71a22aa720e6d7db
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-01-27 10:09:19 -0800

    fixed count

--- lab03_fib.py
+++ lab03_fib.py
@@ -6,20 +6,23 @@ B = pyrtl.Input(bitwidth=32, name='B')
 regA = pyrtl.Register(bitwidth=32, name='regA')
 regResult = pyrtl.Register(bitwidth=32, name='regResult')
 
-count = pyrtl.Input(bitwidth=32, name='count')
+count = pyrtl.Register(bitwidth=32, name='count')
 
 result = pyrtl.Output(bitwidth=32, name='result')
 
 with pyrtl.conditional_assignment:
     with count == 0:
         regA.next |= A
+        count.next |= count + 1
         result |= A
     with count == 1:
         regResult.next |= B
+        count.next |= count + 1
         result |= B
     with count > 1:
         regResult.next |= regA + regResult
         regA.next |= regResult
+        count.next |= count + 1
         result |= regA + regResult
 
 sim_trace = pyrtl.SimulationTrace()
@@ -28,7 +31,6 @@ sim = pyrtl.Simulation(tracer=sim_trace)
 for cycle in range(16):
     sim.step({
         'A': 1,
-        'B': 2,
-        'count': cycle
+        'B': 2
     })
 sim_trace.render_trace()
